71fdff436d192063ffb92ca0f54300cb344c3785
Julius Ikkala <julius.ikkala@gmail.com>
2018-08-01 22:51:26 +0300

    And string output for Python, JS and Bash

--- img2string.c
+++ img2string.c
@@ -1,21 +1,21 @@
 /*
-    Copyright 2018 Julius Ikkala
-
-    This file is part of img2string.
-
-    img2string is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    img2string is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with img2string.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ *  Copyright 2018 Julius Ikkala
+ *
+ *  This file is part of img2string.
+ *
+ *  img2string is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  img2string is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with img2string.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <stdio.h>
 #include <caca.h>
 #include <getopt.h>
@@ -29,7 +29,22 @@
 #define GAMMA 'g'
 #define DITHER 'd'
 #define ANTIALIAS 'a'
-#define PREVIEW 'p'
+#define OUTPUT 'o'
+
+enum output_mode
+{
+    OUTPUT_STDOUT = 0,
+    OUTPUT_C,
+    OUTPUT_PYTHON,
+    /* Specifically, node.js compatible Javascript since ANSI escape codes
+     * wouldn't make sense elsewhere.
+     */
+    OUTPUT_JAVASCRIPT,
+    /* You'll have to print it like this:
+     * echo -e "$IMAGE"
+     */
+    OUTPUT_BASH
+};
 
 struct
 {
@@ -39,8 +54,8 @@ struct
     const char* image;
     const char* dither;
     int antialias;
-    int preview;
-} options = { 80, 0.5f, 0.5f, NULL, "none", 0, 0 };
+    enum output_mode output;
+} options = { 80, 0.5f, 0.5f, NULL, "none", 0, OUTPUT_STDOUT };
 
 int parse_args(int argc, char** argv)
 {
@@ -53,25 +68,41 @@ int parse_args(int argc, char** argv)
         { "ratio", required_argument, NULL, RATIO },
         { "gamma", required_argument, NULL, GAMMA },
         { "dither", required_argument, NULL, DITHER },
-        { "preview", no_argument, &options.preview, PREVIEW }
+        { "antialias", no_argument, &options.antialias, ANTIALIAS },
+        { "output", required_argument, NULL, OUTPUT }
     };
 
     int val = 0;
-    while((val = getopt_long(argc, argv, "w:r:g:d:ap", longopts, &indexptr)) != -1)
+    while((val = getopt_long(argc, argv, "w:r:g:d:ao:", longopts, &indexptr)) != -1)
     {
         switch(val)
         {
         case WIDTH:
             options.width = strtoul(optarg, &endptr, 10);
-            if(*endptr != 0) goto help_print;
+
+            if(*endptr != 0)
+            {
+                printf("Width must be a positive integer\n");
+                goto help_print;
+            }
             break;
         case RATIO:
             options.ratio = strtod(optarg, &endptr);
-            if(*endptr != 0) goto help_print;
+
+            if(*endptr != 0)
+            {
+                printf("Ratio must be a number\n");
+                goto help_print;
+            }
             break;
         case GAMMA:
             options.gamma = strtod(optarg, &endptr);
-            if(*endptr != 0) goto help_print;
+
+            if(*endptr != 0)
+            {
+                printf("Gamma must be a number\n");
+                goto help_print;
+            }
             break;
         case DITHER:
             if(
@@ -81,14 +112,29 @@ int parse_args(int argc, char** argv)
                 strcmp(optarg, "ordered8") != 0 &&
                 strcmp(optarg, "random") != 0 &&
                 strcmp(optarg, "fstein") != 0
-            ) goto help_print;
+            ) {
+                printf("Unknown dither %s\n", optarg);
+                goto help_print;
+            }
             options.dither = optarg;
             break;
         case ANTIALIAS:
             options.antialias = 1;
             break;
-        case PREVIEW:
-            options.preview = 1;
+        case OUTPUT:
+            if(!strcmp(optarg, "s") || !strcmp(optarg, "stdout"))
+                options.output = OUTPUT_STDOUT;
+            else if(!strcmp(optarg, "c")) options.output = OUTPUT_C;
+            else if(!strcmp(optarg, "py") || !strcmp(optarg, "python"))
+                options.output = OUTPUT_PYTHON;
+            else if(!strcmp(optarg, "js") || !strcmp(optarg, "javascript"))
+                options.output = OUTPUT_JAVASCRIPT;
+            else if(!strcmp(optarg, "sh") || !strcmp(optarg, "bash"))
+                options.output = OUTPUT_BASH;
+            else {
+                printf("Unknown output mode %s\n", optarg);
+                goto help_print;
+            }
             break;
         case HELP:
             goto help_print;
@@ -106,16 +152,23 @@ int parse_args(int argc, char** argv)
 
 help_print:
     printf(
-        "Usage: %s [-w width] [-g gamma] [-d dither] [-r pixelratio] [-a] [-p] image\n"
-        "dither can be one of the following:\n"
+        "Usage: %s [-w width] [-g gamma] [-d dither] [-r pixelratio] "
+        "[-o mode] [-a] image\n"
+        "\ndither can be one of the following:\n"
         "\tnone (default)\n"
         "\tordered2\n"
         "\tordered4\n"
         "\tordered8\n"
         "\trandom\n"
         "\tfstein\n"
-        "pixelratio is the aspect ratio of a pixel.\n"
-        "-a enables antialiasing.\n",
+        "\npixelratio is the aspect ratio of a pixel.\n"
+        "\nmode is the output mode. It can be one of the following:\n"
+        "\ts \tShows the result directly.\n"
+        "\tc \tOutputs a C string.\n"
+        "\tpy\tOutputs a Python string.\n"
+        "\tjs\tOutputs a Javascript (node.js compatible) string.\n"
+        "\tsh\tOutputs a Bash string.\n"
+        "\n-a enables antialiasing.\n",
         argv[0]
     );
 return 1;
@@ -147,81 +200,136 @@ int caca_to_ansi(enum caca_color color)
     };
 }
 
-void print_canvas(caca_canvas_t* cv)
+#define nop
+
+#define output_canvas_pixels(cv, pre, post, reset, line_start, line_end, output, set_fg, set_both) { \
+    int w = caca_get_canvas_width(cv); \
+    int h = caca_get_canvas_height(cv); \
+    pre;\
+    for(int y= 0; y < h; ++y) { \
+        uint32_t prev_a = 0; \
+        line_start; \
+        for(int x = 0; x < w; ++x) { \
+            char c = caca_get_char(cv, x, y); \
+            uint32_t a = caca_get_attr(cv, x, y); \
+            if(a != prev_a) { \
+                uint8_t fg = caca_attr_to_ansi_fg(a); \
+                uint8_t bg = caca_attr_to_ansi_bg(a); \
+                if(bg == CACA_TRANSPARENT) { \
+                    reset; \
+                    if(fg != CACA_TRANSPARENT) set_fg(caca_to_ansi(fg)); \
+                } else set_both(caca_to_ansi(fg), caca_to_ansi(bg)+10); \
+            } \
+            output(c); \
+            prev_a = a; \
+        } \
+        reset; \
+        line_end; \
+    } \
+    post; \
+}
+
+#define stdout_fg(fg) printf("\x1b[%dm", fg)
+#define stdout_both(fg, bg) printf("\x1b[%d;%dm", fg, bg)
+
+void print_canvas_stdout(caca_canvas_t* cv) output_canvas_pixels(
+    cv,
+    printf("\x1b[0m"),
+    nop,
+    printf("\x1b[0m"),
+    nop,
+    printf("\x1b[0m\n"),
+    putchar,
+    stdout_fg,
+    stdout_both
+);
+
+void print_escaped_char(char c)
 {
-    int w = caca_get_canvas_width(cv);
-    int h = caca_get_canvas_height(cv);
-
-    printf("\x1b[0m");
-    for(int y= 0; y < h; ++y)
+    switch(c)
     {
-        uint32_t prev_a = 0;
-        for(int x = 0; x < w; ++x)
-        {
-            char c = caca_get_char(cv, x, y);
-            uint32_t a = caca_get_attr(cv, x, y);
-            if(a != prev_a)
-            {
-                uint8_t fg = caca_attr_to_ansi_fg(a);
-                uint8_t bg = caca_attr_to_ansi_bg(a);
-
-                if(bg == CACA_TRANSPARENT)
-                {
-                    printf("\x1b[0m");
-                    if(fg != CACA_TRANSPARENT)
-                        printf("\x1b[%dm", caca_to_ansi(fg));
-                }
-                else printf(
-                    "\x1b[%d;%dm",
-                    caca_to_ansi(fg),
-                    caca_to_ansi(bg)+10
-                );
-            }
-            putchar(c);
-            prev_a = a;
-        }
-        printf("\x1b[0m\n");
+    case '\"':
+    case '\'':
+    case '\?':
+    case '\\':
+        printf("\\%c", c);
+        break;
+    default:
+        putchar(c);
+        break;
     }
 }
 
-void stringify_canvas(caca_canvas_t* cv)
+#define stringout_fg(fg) printf("\\x1b[%dm", fg)
+#define stringout_both(fg, bg) printf("\\x1b[%d;%dm", fg, bg)
+
+void print_canvas_c(caca_canvas_t* cv) output_canvas_pixels(
+    cv,
+    printf("const char* image = \"\\x1b[0m"),
+    printf("\";\n");,
+    printf("\\x1b[0m"),
+    nop,
+    printf("\\x1b[0m\\n"),
+    print_escaped_char,
+    stringout_fg,
+    stringout_both
+);
+
+void print_canvas_python(caca_canvas_t* cv) output_canvas_pixels(
+    cv,
+    printf("image = \"\"\"\\x1b[0m"),
+    printf("\"\"\"\n");,
+    printf("\\x1b[0m"),
+    nop,
+    printf("\\x1b[0m\n"),
+    print_escaped_char,
+    stringout_fg,
+    stringout_both
+);
+
+void print_canvas_javascript(caca_canvas_t* cv) output_canvas_pixels(
+    cv,
+    printf("const image = \'\\x1b[0m"),
+    printf("\';\n");,
+    printf("\\x1b[0m"),
+    nop,
+    printf("\\x1b[0m\\n"),
+    print_escaped_char,
+    stringout_fg,
+    stringout_both
+);
+
+void print_escaped_char_bash(char c)
 {
-    int w = caca_get_canvas_width(cv);
-    int h = caca_get_canvas_height(cv);
-
-    printf("\"\\x1b[0m");
-    for(int y= 0; y < h; ++y)
+    switch(c)
     {
-        if(y != 0) putchar('\"');
-
-        uint32_t prev_a = 0;
-        for(int x = 0; x < w; ++x)
-        {
-            char c = caca_get_char(cv, x, y);
-            uint32_t a = caca_get_attr(cv, x, y);
-            if(a != prev_a)
-            {
-                uint8_t fg = caca_attr_to_ansi_fg(a);
-                uint8_t bg = caca_attr_to_ansi_bg(a);
-
-                if(bg == CACA_TRANSPARENT)
-                {
-                    printf("\\x1b[0m");
-                    if(fg != CACA_TRANSPARENT)
-                        printf("\\x1b[%dm", caca_to_ansi(fg));
-                }
-                else printf(
-                    "\\x1b[%d;%dm",
-                    caca_to_ansi(fg),
-                    caca_to_ansi(bg)+10
-                );
-            }
-            putchar(c);
-            prev_a = a;
-        }
-        printf("\\x1b[0m\\n\"\n");
+    case '\"':
+    case '\\':
+    case '$':
+    case '`':
+    case '!':
+        printf("\\%c", c);
+        break;
+    default:
+        putchar(c);
+        break;
     }
 }
+
+#define bash_fg(fg) printf("\\033[%dm", fg)
+#define bash_both(fg, bg) printf("\\033[%d;%dm", fg, bg)
+
+void print_canvas_bash(caca_canvas_t* cv) output_canvas_pixels(
+    cv,
+    printf("IMAGE=\"\\033[0m"),
+    printf("\"\n");,
+    printf("\\033[0m"),
+    nop,
+    printf("\\033[0m\\n"),
+    print_escaped_char_bash,
+    bash_fg,
+    bash_both
+);
 
 int main(int argc, char** argv)
 {
@@ -239,7 +347,7 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    int height = (int)(options.width*options.ratio*in_h/in_w);
+    int height = (int)round(options.width*options.ratio*in_h/in_w);
 
     caca_canvas_t* canvas = caca_create_canvas(options.width, height);
     caca_set_color_ansi(canvas, CACA_TRANSPARENT, CACA_TRANSPARENT);
@@ -268,8 +376,24 @@ int main(int argc, char** argv)
         input_data
     );
 
-    if(options.preview) print_canvas(canvas);
-    else stringify_canvas(canvas);
+    switch(options.output)
+    {
+    case OUTPUT_STDOUT:
+        print_canvas_stdout(canvas);
+        break;
+    case OUTPUT_C:
+        print_canvas_c(canvas);
+        break;
+    case OUTPUT_PYTHON:
+        print_canvas_python(canvas);
+        break;
+    case OUTPUT_JAVASCRIPT:
+        print_canvas_javascript(canvas);
+        break;
+    case OUTPUT_BASH:
+        print_canvas_bash(canvas);
+        break;
+    }
 
     caca_free_dither(dither);
     caca_free_canvas(canvas);
